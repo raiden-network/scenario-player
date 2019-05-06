194bfd3188974d8772928e0c86617cdc9c8ed3f5
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-06 09:47:41 +0200

    Allow default table, encoding, decoding options.
    Adjust TestRedis class accordingly.

--- raiden/scenario_player/services/utils/testing.py
+++ raiden/scenario_player/services/utils/testing.py
@@ -19,7 +19,10 @@ class TestRedis(MutableMapping):
     """
     DB = {}
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, table, *args, encoding_options=None, decoding_options=None, **kwargs):
+        self.table = table
+        self.encoding_options = encoding_options.items()
+        self.decoding_options = decoding_options.items()
         self.args = args
         self.kwargs = kwargs
 
@@ -38,13 +41,21 @@ class TestRedis(MutableMapping):
     def __delitem__(self, key):
         return super(TestRedis, self).__delitem__(key)
 
-    def set_json(self, table, key, value, **encode_kwargs):
+    def tget(self, key, *args, **decode_kwargs):
+        decode_ops = dict(self.decoding_options)
+        decode_ops.update(decode_kwargs)
+        return self.get_json(self.table, key, *args, **decode_ops)
 
+    def tset(self, key, value, **encode_kwargs):
+        encode_ops = dict(self.encoding_options)
+        enccode_ops.update(encode_kwargs)
+        return self.set_json(self.table, key, value, **encode_ops)
+
+    def set_json(self, table, key, value, **encode_kwargs):
         json_string = json.dumps(value, **encode_kwargs)
         self.__setitem__(table, {key: json_string})
 
     def get_json(self, table, key, *get_args, **decode_kwargs):
-
         json_string = self.__getitem__(table, key)
         decoded = json.loads(json_string, **decode_kwargs)
         return decoded
