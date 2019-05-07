969f151f1c24b44ffce91581514f7359832e3e0a
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-07 15:44:05 +0200

    Split scenarios service.

---
+++ raiden/scenario_player/services/runner/blueprints/__init__.py
@@ -0,0 +1,1 @@
+from raiden.scenario_player.services.runner.blueprints.runner import runner_view
---
+++ raiden/scenario_player/services/scenario/blueprints/__init__.py
@@ -0,0 +1,3 @@
+from raiden.scenario_player.services.scenario.blueprints.scenario import scenario_view
+from raiden.scenario_player.services.scenario.blueprints.runner import runner_view
+from raiden.scenario_player.services.scenario.blueprints.validate import validate_view
