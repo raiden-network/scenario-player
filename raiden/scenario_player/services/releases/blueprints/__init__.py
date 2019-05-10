f67676c2fcc32642c74acf1c9ea11ce125319da6
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-10 14:32:11 +0200

    SP no longer a namespace package.

--- raiden/scenario_player/services/releases/blueprints/__init__.py
+++
@@ -1,4 +0,0 @@
-from raiden.scenario_player.services.releases.blueprints.releases import releases_views
-from raiden.scenario_player.services.releases.blueprints.archives import archives_views
-from raiden.scenario_player.services.releases.blueprints.binaries import binaries_views
-
