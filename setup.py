a7bad8365096593dac70249ccbe78d9c193e1d86
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-10 14:32:58 +0200

    No longer provide scenario files in this package.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 setup(
     name="raiden-scenario-player",
     packages=find_packages(),
-    package_data={"": ["*.yaml", "/*.yml"]},
     entry_points={"console_scripts": ["scenario-player=scenario_player.__main__:main"]},
     install_requires=[
         "raiden>=0.100.3",
