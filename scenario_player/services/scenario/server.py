210a72c43eb418d4f62904748aa4f29fa2a260f5
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-13 12:00:24 +0200

    Fix import of app constructor.

--- scenario_player/services/scenario/server.py
+++ scenario_player/services/scenario/server.py
@@ -1,5 +1,5 @@
 from scenario_player.services.scenario.blueprints import scenario_view, runner_view, validate_view
-from scenario_player.services.utils import construct_flask_app
+from scenario_player.services.common.factories import construct_flask_app
 
 
 def create_scenario_service(test_config=None, secret='dev'):
