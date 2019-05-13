6c8f09c3c4aa8fbe82f72619362b4759e4e2f7e4
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-13 10:18:25 +0200

    Update init imports.

--- scenario_player/services/releases/blueprints/__init__.py
+++ scenario_player/services/releases/blueprints/__init__.py
@@ -1,2 +1,3 @@
+from scenario_player.services.releases.blueprints.archives import archives_views
 from scenario_player.services.releases.blueprints.binaries import binaries_views
-
+from scenario_player.services.releases.blueprints.releases import releases_views
