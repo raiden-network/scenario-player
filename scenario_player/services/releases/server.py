c9c8ccb1ae9ad331e7cf099d4e22ca816f6bdbc6
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-13 10:18:47 +0200

    Update imports.

--- scenario_player/services/releases/server.py
+++ scenario_player/services/releases/server.py
@@ -1,8 +1,8 @@
-from scenario_player.services.releases.blueprints import releases_view, binaries_view, archives_view
-from scenario_player.services.utils import construct_flask_app
+from scenario_player.services.releases.blueprints import archives_views, binaries_views, releases_views
+from scenario_player.services.common.factories import construct_flask_app
 
 
 def create_release_service(test_config=None, secret='dev'):
     return construct_flask_app(
-        releases_view, archives_view, binaries_view, test_config=test_config, secret=secret, db_name='releases'
+        archives_views, binaries_views, releases_views, test_config=test_config, secret=secret, db_name='releases'
     )
