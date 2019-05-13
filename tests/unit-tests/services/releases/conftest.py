5702799504777d47dd3edf5ec5966373b084f9e9
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-05-13 13:48:54 +0200

    Fix syntax issues, imports

--- tests/unit-tests/services/releases/conftest.py
+++ tests/unit-tests/services/releases/conftest.py
@@ -6,4 +6,4 @@ import pytest
 @pytest.fixture
 def tmpdir_path(tmpdir):
     """Return the tmpdir fixture as a pathlib.Path object."""
-    return pathlib.Path(tmpdir.abspath())
+    return pathlib.Path(str(tmpdir.realpath()))
