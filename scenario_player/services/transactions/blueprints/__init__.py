2aa9e1e97393041c54b4210c1b354ec28e0dfccf
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-06-26 09:11:41 +0200

    Import blueprints from sub-modules.

---
+++ scenario_player/services/transactions/blueprints/__init__.py
@@ -0,0 +1,2 @@
+from scenario_player.services.transactions.blueprints.transactions import transactions_blueprint
+from scenario_player.services.transactions.blueprints.tokens import tokens_blueprint
