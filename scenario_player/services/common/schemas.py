27c17eb09d2fd37e04f4fea9fc0a02bac72b742f
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-06-26 18:00:50 +0200

    Implement _serialize, _deserialize of field.
    Also:
    - update its _validate method.
    - update ValidatorSchema.validate_and_deserialize's name
    to reflect its purpose correctly.

--- scenario_player/services/common/schemas.py
+++ scenario_player/services/common/schemas.py
@@ -11,22 +11,28 @@ class ValidatorSchema(Schema):
     Provides a convenience method for validation and deserialization.
     """
 
-    def validate_and_serialize(self, data_obj):
+    def validate_and_deserialize(self, data_obj):
         """Validate `data_obj` and deserialize its fields to native python objects.
 
         If validation fails, this calls `flask.abort(400, str(<dict of errors>)`,
         ending the request sequence.
         """
-        errors = self.validate(data_obj)
-        if errors:
+        try:
+            return self.load(data_obj).data
+        except ValidationError:
             flask.abort(400, str(errors))
         return self.load(data_obj).data
 
 
 class BytesField(Field):
     def _validate(self, value):
-        if type(value) is not bytes:
+        try:
+            value.encode('UTF-8')
+        except (UnicodeEncodeError, AttributeError):
             raise ValidationError('Invalid input type.')
 
-        if value is None or value == b'':
-            raise ValidationError('Invalid BytesField value!')
+    def _deserialize(self, value: str, attr, data):
+        return value.encode('utf-8')
+
+    def _serialize(self, value: bytes, attr, obj):
+        return value.decode('utf-8')
