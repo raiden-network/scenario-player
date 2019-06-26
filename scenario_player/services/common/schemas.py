ce08dfdea495c5fd33fc0471c3989c8ee0a83780
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-06-26 18:00:50 +0200

    Implement _serialize, _deserialize of field.
    Also:
    - update its _validate method.
    - update ValidatorSchema.validate_and_deserialize's name
    to reflect its purpose correctly.
    - Drop SerializerSchema class, as that was a brainfart.

--- scenario_player/services/common/schemas.py
+++ scenario_player/services/common/schemas.py
@@ -11,37 +11,28 @@ class ValidatorSchema(Schema):
     Provides a convenience method for validation and deserialization.
     """
 
-    def dump(self, *args, **kwargs):
-        raise SyntaxError(f"ValidatorSchema subclasses cannot be used to dump data! {self.__class__.__qualname__}")
-
-    def dumps(self, *args, **kwargs):
-        raise SyntaxError(f"ValidatorSchema subclasses cannot be used to dump data! {self.__class__.__qualname__}")
-
-    def validate_and_serialize(self, data_obj):
+    def validate_and_deserialize(self, data_obj):
         """Validate `data_obj` and deserialize its fields to native python objects.
 
         If validation fails, this calls `flask.abort(400, str(<dict of errors>)`,
         ending the request sequence.
         """
-        errors = self.validate(data_obj)
-        if errors:
+        try:
+            return self.load(data_obj).data
+        except ValidationError:
             flask.abort(400, str(errors))
         return self.load(data_obj)
 
 
-class SerializerSchema(Schema):
-    """A modified :class:`marshmallow.Schema` class, disallowing loading of data using one of its instances."""
-    def load(self, *args, **kwargs):
-        raise SyntaxError(f"SerializerSchema subclasses cannot be used to load data! {self.__class__.__qualname__}")
-
-    def loads(self, *args, **kwargs):
-        raise SyntaxError(f"SerializerSchema subclasses cannot be used to load data! {self.__class__.__qualname__}")
-
-
 class BytesField(Field):
     def _validate(self, value):
-        if type(value) is not bytes:
+        try:
+            value.encode('UTF-8')
+        except (UnicodeEncodeError, AttributeError):
             raise ValidationError('Invalid input type.')
 
-        if value is None or value == b'':
-            raise ValidationError('Invalid BytesField value!')
+    def _deserialize(self, value: str, attr, data):
+        return value.encode('utf-8')
+
+    def _serialize(self, value: bytes, attr, obj):
+        return value.decode('utf-8')
