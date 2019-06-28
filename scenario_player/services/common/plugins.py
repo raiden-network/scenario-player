b1577091258e23fc82ee7df436aca78aef4553b8
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-06-28 13:14:22 +0200

    Register our hooks on import.

--- scenario_player/services/common/plugins.py
+++ scenario_player/services/common/plugins.py
@@ -1,26 +1,47 @@
+import sys
 from typing import List, Union
 
 import flask
 import pluggy
 
+#: The namespace plugins should use as a prefix when creating a :class:`pluggy.HookimplMarker`.
+HOST_NAMESPACE = "scenario_player"
 
-def get_plugin_manager():
+#: A list of all blueprints currently registered with the library.
+PLUGIN_BLUEPRINTS = [blueprints for blueprints in pm.hook.register_blueprints() if blueprints]
+
+#: Hook Specification Object for specifying new hooks.
+HOOK_SPEC = pluggy.hooks.HookspecMarker(HOST_NAMESPACE)
+
+#: Hook Implementer object for implementing available hooks.
+HOOK_IMPL = pluggy.hooks.HookimplMarker(HOST_NAMESPACE)
+
+
+def get_plugin_manager(namespace):
     """Fetch pluggy's plugin manager for our library."""
-    pm = pluggy.PluginManager("bitex")
-    pm.add_hookspecs(specs)
-    pm.load_setuptools_entrypoints("bitex")
-    pm.register(base)
+    pm = pluggy.PluginManager(namespace)
+    pm.add_hookspecs(HOOK_SPEC)
+    pm.load_setuptools_entrypoints(namespace)
+    pm.register(namespace)
     return pm
 
 
-pm = get_plugin_manager()
+def register_hook_specifications(library):
+    pm = get_plugin_manager(library)
+    # load all hookimpls from the local module's namespace
+    pm.add_hookspecs(sys.modules[library)
 
 
-PLUGIN_BLUEPRINTS = [blueprints for blueprints in pm.hook.register_blueprints() if blueprints]
-HOST_NAMESPACE = "raiden-scenario-player"
-hook_spec = pluggy.hooks.HookspecMarker(HOST_NAMESPACE)
+def register_hook_implementations(library):
+    pm = get_plugin_manager(library)
+    # load all hookimpls from the local module's namespace
+    pm.register(sys.modules[library)
 
 
-@hook_spec
+@HOOK_SPEC
 def register_blueprints() -> Union[None, List[flask.Blueprint]]:
     """Register a list of blueprints with :mode:`raiden-scenario-player`."""
+
+
+register_hook_specifications(HOST_NAMESPACE)
+register_hook_implementations(HOST_NAMESPACE)
