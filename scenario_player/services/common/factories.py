21670cd0c0b6ccededaa36e969e7e01467c300ef
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-06-28 09:08:45 +0200

    Enable registering blueprints via pluggy.

--- scenario_player/services/common/factories.py
+++ scenario_player/services/common/factories.py
@@ -3,6 +3,7 @@ from typing import List, Mapping
 import flask
 
 from scenario_player.services.common.blueprints import admin_blueprint, metrics_blueprint
+from scenario_player.services.common.plugins import PLUGIN_BLUEPRINTS
 
 
 def attach_blueprints(app: flask.Flask, *blueprints: List[flask.Blueprint]):
@@ -13,7 +14,7 @@ def attach_blueprints(app: flask.Flask, *blueprints: List[flask.Blueprint]):
 
 
 def construct_flask_app(
-    *blueprints: List[flask.Blueprint], db_name: str="default", test_config: Mapping=None, secret: str="dev", config_file: str="config.py"
+    db_name: str="default", test_config: Mapping=None, secret: str="dev", config_file: str="config.py", enable_plugins: bool=True,
 ) -> flask.Flask:
     """Construct a flask app with the given blueprints registered.
 
@@ -40,5 +41,10 @@ def construct_flask_app(
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    attach_blueprints(app, metrics_blueprint, admin_blueprint, *blueprints)
+    attach_blueprints(app, metrics_blueprint, admin_blueprint)
+
+    if enable_plugins:
+        for blueprints in PLUGIN_BLUEPRINTS:
+            attach_blueprints(app, *blueprints)
+
     return app
---
+++ scenario_player/services/common/plugins.py
@@ -0,0 +1,26 @@
+from typing import List, Union
+
+import flask
+import pluggy
+
+
+def get_plugin_manager():
+    """Fetch pluggy's plugin manager for our library."""
+    pm = pluggy.PluginManager("bitex")
+    pm.add_hookspecs(specs)
+    pm.load_setuptools_entrypoints("bitex")
+    pm.register(base)
+    return pm
+
+
+pm = get_plugin_manager()
+
+
+PLUGIN_BLUEPRINTS = [blueprints for blueprints in pm.hook.register_blueprints() if blueprints]
+PLUGIN_NAMESPACE = "raiden-scenario-player"
+hook_spec = pluggy.hooks.HookSpec("raiden-scenario-player")
+
+
+@hook_spec
+def register_blueprints() -> Union[None, List[flask.Blueprint]]:
+    """Register a list of blueprints with :mode:`raiden-scenario-player`."""
