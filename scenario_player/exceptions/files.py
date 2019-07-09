52c4ccf03e9c6f7d719fd6e545ee38985a7404cf
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-07-09 08:29:07 +0200

    Add ReferenceDropped Warning.

--- scenario_player/exceptions/files.py
+++ scenario_player/exceptions/files.py
@@ -5,3 +5,11 @@ class CannotImplicitlyChangeFileType(FileExistsError):
 
     The existing symlink or hard-copy must first be removed explicitly.
     """
+
+
+class ReferenceDropped(UserWarning):
+    """While updating references, we found a file that changed on disk and thusly dropped it."""
+
+    def __init__(self, ref, attr):
+        message = f"Reference {ref} changed on disk - dropping it from '{attr}'."
+        super(ReferenceDropped, self).__init__(message)
