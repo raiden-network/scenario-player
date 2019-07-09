557deaae6043a3ee2e5ce7d34c6aa4fd5feb136a
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-07-09 10:08:41 +0200

    Add expected pattern to docstring.

--- tests/unit-tests/utils/files/test_parsing.py
+++ tests/unit-tests/utils/files/test_parsing.py
@@ -40,10 +40,13 @@ def test_parse_func_always_parses_correctly_independent_of_version_platform_and_
         version, arch, platform, parse_func, expected, tmp_path):
     """Make sure the parsing function always parses the expected value from a given path.
 
+    The pattern expected is raiden_<version>_<platform>_<architecture>[.<ext>]
+
     This assumes a valid pattern for the `path`'s file name.
     """
     fname_sans_ext = f"raiden_{version}_{platform}_{arch}"
     fname_with_ext = f"raiden_{version}_{platform}_{arch}.extension"
+
     for fname in (fname_sans_ext, fname_with_ext):
         assert parse_func(tmp_path.joinpath('some_dir', fname)) == locals().get(expected, None)
 
