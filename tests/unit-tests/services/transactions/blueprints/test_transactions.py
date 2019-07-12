5cdab7c0dea6199e4789ac460c37c6dbf2c5513c
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-07-12 14:50:03 +0200

    Fix incorrect assertions, test setup.

--- tests/unit-tests/services/transactions/blueprints/test_transactions.py
+++ tests/unit-tests/services/transactions/blueprints/test_transactions.py
@@ -1,9 +1,9 @@
 from unittest.mock import patch
+
 import pytest
+
 from raiden.network.rpc import client
 from scenario_player.services.utils.factories import construct_flask_app
-
-import logging
 
 
 @pytest.fixture
@@ -12,14 +12,14 @@ def transaction_service_app():
         app = construct_flask_app()
         app.config['TESTING'] = True
         app.config['rpc-client'] = client.JSONRPCClient(web3='web3_instance', privkey=b"my_private_key")
+        app.config['rpc-client'].send_transaction.return_value = b"result"
+
         yield app
 
 
 @pytest.fixture
 def transaction_service_client(transaction_service_app):
-    client = transaction_service_app.test_client()
-
-    return client
+    return transaction_service_app.test_client()
 
 
 @pytest.mark.dependency(name="transaction_blueprint_loaded")
@@ -33,21 +33,21 @@ class TestNewTransactionEndpoint:
     @pytest.mark.parametrize(
         'parameters, expected_status',
         argvalues=[
-            ({"to": 'someaddress', "startgas": 2}, '400'),
-            ({"value": 123, "startgas": 2}, '400'),
-            ({"value": 123, "to": 'someaddress'}, '400'),
-            ({"value": 123, "to": 'someaddress', "startgas": 2}, '200'),
-            ({"value": 'wholesome', "to": 'someaddress', "startgas": 2}, '400'),
-            ({"value": 123, "to": 55, "startgas": 2}, '400'),
-            ({"value": 123, "to": 'someaddress', "startgas": 'hello'}, '400'),
+            ({"to": 'someaddress', "startgas": 2.0}, '400'),
+            ({"value": 123.0, "startgas": 2.0}, '400'),
+            ({"value": 123.0, "to": 'someaddress'}, '400'),
+            ({"value": 123.0, "to": 'someaddress', "startgas": 2.0}, '200'),
+            ({"value": 'wholesome', "to": 'someaddress', "startgas": 2.0}, '400'),
+            ({"value": 123.0, "to": 55.0, "startgas": 2.0}, '400'),
+            ({"value": 123.0, "to": 'someaddress', "startgas": 'hello'}, '400'),
         ],
         ids=[
             "Missing 'value'",
-            "Missing 'to_address'",
+            "Missing 'to'",
             "Missing 'startgas'",
             "None missing - correct types",
             "None missing - 'value' type incorrect",
-            "None missing - 'to_address' type incorrect",
+            "None missing - 'to' type incorrect",
             "None missing - 'startgas' type incorrect",
         ]
     )
@@ -58,52 +58,83 @@ class TestNewTransactionEndpoint:
 
         These are as follows:
 
-            - `to_address` - a transaction hash, sent as a  `utf-8` encoded :class:`str`.
+            - `to` - a transaction hash, sent as a  `utf-8` encoded :class:`str`.
             - `startgas` - the starting gas to use, as either :class:`int` or :class:`float`.
             - `amount` - the amount to send with the transaction, as either :class:`int` or :class:`float`.
 
         If either one or more is missing, or has an incorrect type, we expect to
         see a `400 Bad Request` response from the service.
         """
-        transaction_service_client.application.config['rpc-client'].send_transaction.return_value = b"tx_hash"
         resp = transaction_service_client.post('/transactions', data=parameters)
 
         assert expected_status in resp.status
 
-    @patch('scenario_player.services.transactions.blueprints.transactions.TransactionSendRequest', autospec=True)
-    def test_new_transaction_calls_load_of_its_validator_schema(
-            self, mock_validator, transaction_service_client
+    @patch('scenario_player.services.transactions.blueprints.transactions.transaction_send_schema')
+    def test_new_transaction_calls_validate_and_deserialize_of_its_schema(
+            self, mock_schema, transaction_service_client
     ):
         """The :meth:`scenario_player.services.transactions.blueprints.TransactionSendRequest.validate_and_deserialize`
         must be called when processing a request.
-        """
-        parameters = {"value": 123, "to_address": 'someaddress', "startgas": 2}
-        mock_validator.load.return_value = parameters
-        transaction_service_client.post('/transactions', data=parameters)
-        mock_validator.load.assert_called_once_with(parameters)
 
-    @patch('scenario_player.services.transactions.blueprints.transactions.TransactionSendRequest', autospec=True)
-    def test_new_transaction_calls_dump_of_its_serializer_schema(self, mock_serializer, transaction_service_client):
+        Since the parameters are passed as a :class:`werkzeug.datastructures.ImmutableMultiDict`, which cannot
+        be compared to other instances created with identical parameters. Hence, we must
+        iterate over the keys of our expected dict and compare manually.
+        """
+        parameters ={"value": 123.0, "to": 'someaddress', "startgas": 2.0}
+        deserialized_parameters = {"value": 123.0, "to": b'someaddress', "startgas": 2.0}
+        mock_schema.configure_mock(
+            **{
+                "validate_and_deserialize.return_value": deserialized_parameters,
+                "dumps.return_value": "ok",
+            }
+        )
+        transaction_service_client.post('/transactions', data=parameters)
+
+        mock_schema.validate_and_deserialize.assert_called_once()
+        args, kwargs = mock_schema.validate_and_deserialize.call_args
+        params_as_multi_dict, *_ = args
+        for key, value in parameters.items():
+            assert key in params_as_multi_dict
+            assert parameters[key] == value
+
+    @patch('scenario_player.services.transactions.blueprints.transactions.transaction_send_schema')
+    def test_new_transaction_calls_dumps_of_its_schema(self, mock_schema, transaction_service_client):
         """The :meth:`scenario_player.services.transactions.blueprints.TransactionSendRequest.dump`
         must be called when processing a request and its result returned by the function.
         """
+        parameters = {"value": 123.0, "to": 'someaddress', "startgas": 2.0}
+        mock_schema.configure_mock(
+            **{
+                "validate_and_deserialize.return_value": parameters,
+                "dumps.return_value": "ok",
+            }
+        )
         expected_tx_hash = b'my_tx_hash'
         transaction_service_client.application.config['rpc-client'].send_transaction.return_value = expected_tx_hash
-        parameters = {"value": 123, "to_address": 'someaddress', "startgas": 2}
-        transaction_service_client.post('/transactions', data=parameters)
-        mock_serializer.dump.assert_called_once_with(expected_tx_hash)
+        parameters = {"value": 123.0, "to": 'someaddress', "startgas": 2.0}
 
-    @patch('scenario_player.services.transactions.blueprints.transactions.TransactionSendRequest', autospec=True)
+        r = transaction_service_client.post('/transactions', data=parameters)
+        assert "200" in r.status
+        mock_schema.dumps.assert_called_once_with({"tx_hash": expected_tx_hash})
+
+    @patch('scenario_player.services.transactions.blueprints.transactions.TransactionSendRequest')
     def test_new_transaction_calls_the_services_jsonrpc_client_with_the_requests_params(
-            self, mock_validator, transaction_service_client
+            self, mock_schema, transaction_service_client
     ):
         """The method :meth:`JSONRPCClient.send_transaction` must be called with the parameters of the request
         and its result passed to :meth:`scenario_player.services.transactions.blueprints.TransactionSendRequest.dump`.
         """
-        parameters = {"value": 123, "to_address": 'someaddress', "startgas": 2}
-        mock_validator.validate_and_deserialize.return_value = parameters
+        parameters = {"value": 123.0, "to": 'someaddress', "startgas": 2.0}
+        deserialized_parameters = {"value": 123.0, "to": b'someaddress', "startgas": 2.0}
+        mock_schema.configure_mock(
+            **{
+                "validate_and_deserialize.return_value": deserialized_parameters,
+                "dumps.return_value": "ok",
+            }
+        )
+
         transaction_service_client.post('/transactions', data=parameters)
-        transaction_service_client.application.config['rpc-client'].send_transaction.assert_called_once_with(parameters)
+        transaction_service_client.application.config['rpc-client'].send_transaction.assert_called_once_with(**deserialized_parameters)
 
     def test_new_transaction_aborts_with_500_if_no_rpc_client_key_exists_in_app_config(
             self, transaction_service_client
@@ -113,11 +144,12 @@ class TestNewTransactionEndpoint:
 
         Specifically, if no `'rpc-client'` key exists in the :attr:`flask.Flask.config` dictionary
         """
-        parameters = {"value": 123, "to_address": 'someaddress', "startgas": 2}
+        parameters = {"value": 123.0, "to": 'someaddress', "startgas": 2.0}
         transaction_service_client.application.config.pop('rpc-client')
+
         resp = transaction_service_client.post('/transactions', data=parameters)
         assert '500' in resp.status
-        assert "No JSONRPCClient instance available on service!" in resp.data
+        assert b"No JSONRPCClient instance available on service!" in resp.data
 
     def test_new_transaction_aborts_with_500_if_rpc_client_key_value_is_not_a_valid_instance_of_rpc_client(
             self, transaction_service_client
@@ -127,8 +159,9 @@ class TestNewTransactionEndpoint:
 
         Specifically, if :var:`flask.Flask.config['rpc-client']` is not an instance of :class:`JSONRPCClient`.
         """
-        parameters = {"value": 123, "to_address": 'someaddress', "startgas": 2}
+        parameters = {"value": 123.0, "to": 'someaddress', "startgas": 2.0}
         transaction_service_client.application.config['rpc-client'] = "This is not a JSONRPCClient instance."
+
         resp = transaction_service_client.post('/transactions', data=parameters)
         assert '500' in resp.status
-        assert "No JSONRPCClient instance available on service!" in resp.data
+        assert b"No JSONRPCClient instance available on service!" in resp.data
