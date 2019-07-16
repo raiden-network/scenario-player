faa1b774e73b39d1e6eb90955c3cb2d9094853a8
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-07-16 18:29:57 +0200

    Do not get ahead of ourselves.

--- scenario_player/services/transactions/blueprints/__init__.py
+++ scenario_player/services/transactions/blueprints/__init__.py
@@ -1,10 +1,9 @@
 import pluggy
 
 from scenario_player.constants import HOST_NAMESPACE
-from scenario_player.services.transactions.blueprints.tokens import tokens_blueprint
 from scenario_player.services.transactions.blueprints.transactions import transactions_blueprint
 
-__all__ = ["tokens_blueprint", "transactions_blueprint"]
+__all__ = ["transactions_blueprint"]
 
 
 HOOK_IMPL = pluggy.HookimplMarker(HOST_NAMESPACE)
@@ -12,5 +11,5 @@ HOOK_IMPL = pluggy.HookimplMarker(HOST_NAMESPACE)
 
 @HOOK_IMPL
 def register_blueprints(app):
-    for bp in (tokens_blueprint, transactions_blueprint):
+    for bp in (transactions_blueprint,):
         app.register_blueprint(bp)
