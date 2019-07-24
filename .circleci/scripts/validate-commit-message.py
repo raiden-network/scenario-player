4bc51cc33d7abfde26d522eb54f6ab50f3a9ace4
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-07-24 19:25:15 +0200

    [FIX-#0] Add debug info.

--- .circleci/scripts/validate-commit-message.py
+++ .circleci/scripts/validate-commit-message.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 
-from constants import COMMIT_TYPE, CURRENT_BRANCH
+from constants import COMMIT_MSG, COMMIT_TYPE, CURRENT_BRANCH
 
+print(f"Validating commit message {COMMIT_MSG!r}")
+print(f"Parsed commit type: {COMMIT_TYPE}")
 if not COMMIT_TYPE:
     # The commit message title does not comply with any of our regexes.
+    print("No commit type parsed - the commit message does not comply with the required pattern!")
     exit(1)
 
 if COMMIT_TYPE == "VERSION_BUMP":
-    exit(0)
-
-if CURRENT_BRANCH == "release" and COMMIT_TYPE == "FEAT":
+    print("Commit by bumpversion tool detected, skipping validation..")
+elif CURRENT_BRANCH == "release" and COMMIT_TYPE == "FEAT":
     print("No feature (FEAT) commits allowed on `release` branches!")
     exit(1)
-
-if CURRENT_BRANCH == "master" and COMMIT_TYPE not in ("HOTFIX", "RELEASE"):
+elif CURRENT_BRANCH == "master" and COMMIT_TYPE not in ("HOTFIX", "RELEASE"):
     print("`master` branch allows  hotfixes (HOTFIX) and releases (RELEASE) only!")
     exit(1)
 
