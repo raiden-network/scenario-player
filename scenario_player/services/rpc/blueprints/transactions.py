4a86d8a01589e2e846a579b560188171bb295782
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-07-30 07:25:49 +0200

    Use client from serialized data.

--- scenario_player/services/rpc/blueprints/transactions.py
+++ scenario_player/services/rpc/blueprints/transactions.py
@@ -11,7 +11,7 @@ The following endpoints are supplied by this blueprint:
         form data.
 
 """
-from flask import Blueprint, Response, current_app, request
+from flask import Blueprint, Response, request
 
 from scenario_player.services.common.metrics import REDMetricsTracker
 from scenario_player.services.rpc.schemas.transactions import TransactionSendRequest
@@ -22,14 +22,14 @@ transactions_blueprint = Blueprint("transactions_view", __name__)
 transaction_send_schema = TransactionSendRequest()
 
 
-@transactions_blueprint.route("/rpc/client/<client_id:rpc-client>/transactions", methods=["POST"])
-def transactions_route(rpc_client):
+@transactions_blueprint.route("/rpc/client/transactions", methods=["POST"])
+def transactions_route(client_id):
     handlers = {"POST": new_transaction}
     with REDMetricsTracker():
-        return handlers[request.method](rpc_client)
+        return handlers[request.method](client_id)
 
 
-def new_transaction(rpc_client):
+def new_transaction(client_id):
     """Create a new transaction.
 
     The given parameters will be passed to the service's
@@ -40,9 +40,10 @@ def new_transaction(rpc_client):
 
     Example::
 
-        POST /rpc/client/<rpc_client_id>/transactions
+        POST /rpc/client/transactions
 
             {
+                "client": <str>,
                 "to": <str>,
                 "startgas": <number>,
                 "value": <number>,
@@ -56,6 +57,7 @@ def new_transaction(rpc_client):
 
     """
     data = transaction_send_schema.validate_and_deserialize(request.form)
+    rpc_client = data["client_id"]
 
     result = rpc_client.send_transaction(**data)
 
