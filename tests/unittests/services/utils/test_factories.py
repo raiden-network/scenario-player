e89a4ac0962e6050f7eb23bffec7385dea44e4d1
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-08-12 12:48:40 +0200

    Style test.

--- tests/unittests/services/utils/test_factories.py
+++ tests/unittests/services/utils/test_factories.py
@@ -7,18 +7,15 @@ from scenario_player.services.utils.factories import construct_flask_app
 
 @pytest.fixture
 def TEST_CONFIG_DICT():
-    return {
-        'TESTING': True,
-        'ICE_CREAM_FLAVOR': 'VANILLA',
-    }
+    return {"TESTING": True, "ICE_CREAM_FLAVOR": "VANILLA"}
 
 
 @pytest.fixture
 def TEST_CONFIG_FILE(tmp_path):
-    config_path = tmp_path.joinpath('config.py')
+    config_path = tmp_path.joinpath("config.py")
     config_path.touch()
-    with config_path.open('w') as f:
-        f.write('TESTING = False\n')
+    with config_path.open("w") as f:
+        f.write("TESTING = False\n")
         f.write('ICE_CREAM_FLAVOR = "CHOCOLATE"\n')
 
     return str(config_path)
@@ -47,23 +44,30 @@ class MockConfig:
 
 
 class TestConstructFlaskApp:
-
-    @pytest.mark.parametrize('test_config', [None, {'TESTING': True}], ids=['With Test Config', 'Without Test Config'])
-    @pytest.mark.parametrize('values', [{'SECRET_KEY': 'dev', 'DATABASE': 'default'}, {}], ids=['defaults', 'overrides'])
-    def test_constructor_always_creates_expected_config_settings_regardless_of_test_config(self, values, test_config):
+    @pytest.mark.parametrize(
+        "test_config", [None, {"TESTING": True}], ids=["With Test Config", "Without Test Config"]
+    )
+    @pytest.mark.parametrize(
+        "values", [{"SECRET_KEY": "dev", "DATABASE": "default"}, {}], ids=["defaults", "overrides"]
+    )
+    def test_constructor_always_creates_expected_config_settings_regardless_of_test_config(
+        self, values, test_config
+    ):
         constructed_app = construct_flask_app(test_config=test_config)
         for key, value in values.items():
             assert constructed_app.config.get(key) == value
 
     def test_test_config_overwrites_app_configs(self):
-        config = {"DATABASE": 'custom_db', 'SECRET_KEY': 'banana pie'}
+        config = {"DATABASE": "custom_db", "SECRET_KEY": "banana pie"}
         app = construct_flask_app(test_config=config)
         for key, value in config.items():
             assert app.config.get(key) == value
 
     @pytest.mark.parametrize("plugins_enabled", [True, False])
-    @mock.patch('scenario_player.services.utils.factories.SP_PM.hook.register_blueprints')
-    def test_constructor_makes_call_to_blueprint_registration_hook(self, mock_register_blueprints, plugins_enabled):
+    @mock.patch("scenario_player.services.utils.factories.SP_PM.hook.register_blueprints")
+    def test_constructor_makes_call_to_blueprint_registration_hook(
+        self, mock_register_blueprints, plugins_enabled
+    ):
         """There is exactly one call for each available Blueprint."""
         construct_flask_app(enable_plugins=plugins_enabled)
         assert mock_register_blueprints.called is plugins_enabled
@@ -71,10 +75,12 @@ class TestConstructFlaskApp:
     def test_loads_config_from_file_if_no_test_config_specified(self, TEST_CONFIG_FILE):
         """The configuration is loaded from a default file if no `test_config` is passed."""
         app = construct_flask_app(config_file=TEST_CONFIG_FILE)
-        assert app.config.get('TESTING') is False
-        assert app.config.get('ICE_CREAM_FLAVOR') == 'CHOCOLATE'
+        assert app.config.get("TESTING") is False
+        assert app.config.get("ICE_CREAM_FLAVOR") == "CHOCOLATE"
 
-    def test_loads_config_from_test_config_instead_of_file_if_test_config_specified(self, TEST_CONFIG_DICT, TEST_CONFIG_FILE):
+    def test_loads_config_from_test_config_instead_of_file_if_test_config_specified(
+        self, TEST_CONFIG_DICT, TEST_CONFIG_FILE
+    ):
         """If a `test_config` dict is passed, we load it, instead of loading from a file."""
         app = construct_flask_app(test_config=TEST_CONFIG_DICT, config_file=TEST_CONFIG_FILE)
         for key, value in TEST_CONFIG_DICT.items():
