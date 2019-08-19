6deb6bffe55b2a5c2110b41bf5456424c06e9677
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-08-19 09:27:17 +0200

    Invoke circleci cmd correctly.

--- .circleci/scripts/skip-on-bad-commit-message.py
+++ .circleci/scripts/skip-on-bad-commit-message.py
@@ -4,4 +4,4 @@ try:
     subprocess.run("python ${CI_SCRIPTS_DIR}/validate-commit-message.py".split(), check=True)
 except subprocess.SubprocessError:
     # The commit message is bogus, so we skip the current step.
-    subprocess.run("circle step halt".split())
+    subprocess.run("circleci step halt".split())
