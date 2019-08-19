e2f53d3512011d922aea67426f7ff526fef1affa
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-08-19 10:54:20 +0200

    Explicitly checkout files, instead of merge.

--- .circleci/scripts/bump-part.py
+++ .circleci/scripts/bump-part.py
@@ -98,5 +98,7 @@ if CURRENT_BRANCH == "master":
     # Sync up our versions between dev and master. The actual bump to dev's tag will be done
     # on the next merge commit to it.
     subprocess.run(f"git --git-dir={PROJECT_GIT_DIR} checkout dev".split(" "), check=True)
-    subprocess.run(f"git --git-dir={PROJECT_GIT_DIR} merge master".split(" "), check=True)
+    subprocess.run(f"git --git-dir={PROJECT_GIT_DIR} git checkout master .bumpversion.cfg".split(" "), check=True)
+    subprocess.run(f"git --git-dir={PROJECT_GIT_DIR} git checkout master scenario_player/__init__.py".split(" "), check=True)
+    subprocess.run(f"git --git-dir={PROJECT_GIT_DIR} git commit .bumpversion.cfg scenario_player/__init__.py -m 'Sync Branch Versions.'".split(" "), check=True)
     subprocess.run(f"git --git-dir={PROJECT_GIT_DIR} push".split(" "), check=True)
