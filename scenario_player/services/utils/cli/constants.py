a8340526d05c40de5875ef017f9fedbf5e78603f
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-08-21 15:32:04 +0200

    Update Constants.

---
+++ scenario_player/services/utils/cli/constants.py
@@ -0,0 +1,21 @@
+import scenario_player.services.rpc.app.serve
+SERVICE_APPS = {"rpc": scenario_player.services.rpc.app.serve}
+
+SERVICE_TEMPLATE = """
+[Unit]
+Description=Scenario-Player-as-a-Service {service} Unit
+After=network.target
+
+[Service]
+Type=simple
+WorkingDirectory={workdir}
+ExecStart={spaas} start --service {service} --host {host} --port {port}
+Restart=always
+
+StandardOutput=syslog
+StandardError=syslog
+SyslogIdentifier=SPaaS-{service}
+
+[Install]
+WantedBy=multi-user.target
+"""
