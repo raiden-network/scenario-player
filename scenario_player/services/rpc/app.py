83a246f8fb7c6aecd0c8210e4ab182709bcf3511
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-08-21 15:35:59 +0200

    Replace service daemon and app constructor.

--- scenario_player/services/rpc/app.py
+++ scenario_player/services/rpc/app.py
@@ -8,7 +8,6 @@ import logging
 import flask
 import structlog
 import waitress
-from daemonize import Daemonize
 
 from scenario_player.services.common.blueprints import admin_blueprint, metrics_blueprint
 from scenario_player.services.rpc.blueprints import (
@@ -17,14 +16,13 @@ from scenario_player.services.rpc.blueprints import (
     transactions_blueprint,
 )
 from scenario_player.services.rpc.utils import RPCRegistry
-from scenario_player.services.utils.factories import default_service_daemon_cli
 
 
-def rpc_app():
-    """Create a :mod:`flask` app using only the RPC blueprints."""
+def serve(parsed):
     from scenario_player import __version__
-
+    logging.basicConfig(filename=".raiden/spaas/rpc.log", filemode="a+", level=logging.DEBUG)
     log = structlog.getLogger()
+
     NAME = "SPaaS-RPC-Service"
 
     log.info("Creating RPC Flask App", version=__version__, name=NAME)
@@ -44,35 +42,6 @@ def rpc_app():
     for bp in blueprints:
         log.debug("Registering blueprint", blueprint=bp.name)
         app.register_blueprint(bp)
-    return app
 
+    waitress.serve(app, host=parsed.host, port=parsed.port)
 
-def service_daemon():
-    parser = default_service_daemon_cli()
-
-    args = parser.parse_args()
-
-    logfile_path = args.raiden_dir.joinpath("spaas")
-    logfile_path.mkdir(exist_ok=True, parents=True)
-    logfile_path = logfile_path.joinpath("SPaaS-RPC.log")
-    logfile_path.touch()
-
-    host, port = args.host, args.port
-
-    def serve_rpc():
-        """Run an RPC flask app as a daemonized process."""
-        logging.basicConfig(filename=logfile_path, filemode="a+", level=logging.DEBUG)
-        log = structlog.getLogger()
-
-        app = rpc_app()
-
-        log.info("Starting RPC Service", host=host, port=port)
-        waitress.serve(app, host=host, port=port)
-
-    PIDFILE = args.raiden_dir.joinpath("spaas", "rpc-service.pid")
-
-    daemon = Daemonize("SPaaS-RPC", PIDFILE, serve_rpc)
-    if args.command == "start":
-        daemon.start()
-    elif args.command == "stop":
-        daemon.exit()
