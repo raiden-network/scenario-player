02efd0caade524e7caf0423b36d8a098a3d29c43
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-08-22 10:27:35 +0200

    Create ChangeLog on Merge commits to master.

--- .circleci/scripts/update-changelog.py
+++ .circleci/scripts/update-changelog.py
@@ -1,9 +1,9 @@
 """TODO: This script is a stub."""
+import pathlib
 import re
 import subprocess
 
-from typing import List, Tuple
-
+from typing import List, Set, Tuple
 from constants import PROJECT_GIT_DIR, CURRENT_BRANCH, COMMIT_PATTERN
 
 
@@ -36,18 +36,21 @@ def read_git_commit_history_since_tag(tag) -> Tuple[List[Tuple[str, str]], List[
         3. hotfix commits
     """
     completed_process = subprocess.run(
-        f"git --git-dir={PROJECT_GIT_DIR} log master..{tag} --format=%s".split(" "),
+        f"git --git-dir={PROJECT_GIT_DIR} log {tag}..master --format=%s".split(" "),
         check=True,
         stdout=subprocess.PIPE,
     )
     titles = completed_process.stdout.decode("UTF-8").split("\n")
 
+    # The body of a commit may include newline characters, so we need to specify
+    # a custom separator to indicate the end of the commit body.
+    separator = "<><><>"
     completed_process = subprocess.run(
-        f"git --git-dir={PROJECT_GIT_DIR} log master..{tag} --format=%b".split(" "),
+        f"git --git-dir={PROJECT_GIT_DIR} log {tag}..master --format=%b{separator}".split(" "),
         check=True,
         stdout=subprocess.PIPE,
     )
-    bodies = completed_process.stdout.decode("UTF-8").split("\n")
+    bodies = completed_process.stdout.decode("UTF-8").split(separator)
 
     assert len(titles) == len(bodies)
 
@@ -71,22 +74,73 @@ def read_git_commit_history_since_tag(tag) -> Tuple[List[Tuple[str, str]], List[
         else:
             print(f"No type found, skipping commit '{title}'..")
 
+    print("feats", feats)
+    print("fixes", fixes)
+    print("hotfixes", hotfixes)
+
     return feats, fixes, hotfixes
 
 
-def format_commits(*commits: List[Tuple[str, str]]) -> List[str]:
+def format_commits(commits: List[Tuple[str, str]]) -> List[str]:
     """Format the given commits for writing to the Changelog.
 
-    The expected input str format is:
+    The expected input Tuple[str, str] format is:
 
-        [(FEAT|FIX|HOTFIX)-#123] <Subject>
-
-        <Optional body with further details on the commit.>
+        ([(FEAT|FIX|HOTFIX)-#123] <Subject>, <Optional body with further details on the commit.>)
 
     The output format is as follows::
 
-        - #123 <Subject>
-            <Optional body with further details on the commit.>
+        r'- #123 <Subject>\n    <Optional body with further details on the commit.>\n'
 
+    Newlines in the body are honored, and each line indented by 4 spaces automatically.
+    TODO: Duplicate Issues should share a single Changelog Entry.
     """
+    if not commits:
+        return []
+    pattern = re.compile(COMMIT_PATTERN)
+    formatted = set()
+    print("UNPACKING", commits)
+    for title, body in commits:
+        match = pattern.match(title)
+        issue, subject = match.groupdict()["ISSUE"], match.groupdict()["SUBJECT"]
 
+        entry = f"- {issue} {subject}\n"
+
+        if body:
+            # Make sure the body is indented by 8 spaces.
+            formatted_body = "        ".join(body.split("\n"))
+            entry += f"{formatted_body}\n"
+        formatted.add(entry)
+    return sorted(formatted)
+
+
+def update_chlog(tag: str, feats: List[str], fixes: List[str], hotfixes: List[str], chlog_path: pathlib.Path = pathlib.Path("CHANGELOG.rst")):
+    try:
+        history = chlog_path.read_text()
+    except FileNotFoundError:
+        print("No Changelog file found - creating a new one.")
+        history = ""
+
+    chlog_entry = f"RELEASE {tag}\n=============\n\n"
+
+    if feats:
+        feats = "\n".join(feats)
+        chlog_entry += f"Features\n--------\n{feats}\n"""
+
+    if fixes:
+        fixes = "\n".join(fixes)
+
+        chlog_entry += f"Fixes\n-----\n{fixes}\n"
+
+    if hotfixes:
+        hotfixes = "\n".join(hotfixes)
+
+        chlog_entry += f"Hotfixes\n--------\n{hotfixes}\n"
+    chlog_path.write_text(f"{chlog_entry}\n{history}")
+
+
+if __name__ == '__main__':
+    import sys
+    chlog_path = pathlib.Path(sys.argv[1])
+    feats, fixes, hotfixes = read_git_commit_history_since_tag("v0.3.0")
+    update_chlog("0.4.0", format_commits(feats), format_commits(fixes), format_commits(hotfixes), chlog_path)
