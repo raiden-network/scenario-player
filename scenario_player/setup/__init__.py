6e5bafce2754b74c41185b0dc350a6b69b141e5b
brainbot-devops <nlsdfnbch.foss@kolabnow.com>
2019-09-24 15:57:37 +0200

    Addressed documentation issues.

--- scenario_player/setup/__init__.py
+++ scenario_player/setup/__init__.py
@@ -88,8 +88,7 @@ SP Account Eth Funding
 If the SP Account does not have sufficient ether, we cannot do anything useful.
 Hence, we check if the SP Account's address has sufficient funding (as defined
 in the scenario yaml). If this is **not** the case, we terminate the scenario run,
-since we do not, as of now, have any effective way of funding an account on a
-test net programmatically.
+since we cannot create ether out of thin air (yet).
 
 Raiden Client Node Eth Funding
 ------------------------------
@@ -111,8 +110,8 @@ SmartContract Operations
 Deploy/Re-use SP Token
 ----------------------
 For our SP to be able to test token transfers, we first need a token to, well, transfer.
-By default, the SP deploys a custom token contract to the specified network, which
-allows minting tokens via rpc call. This means each run starts with a fresh token economy.
+By default, the SP deploys a custom token contract to the specified network, which contains
+a function to mint tokens on demand. This means each run starts with a fresh token economy.
 
 If this is not desired, it's also possible to re-use an existing SP Token ontract.
 This must be stated in the scenario yaml, and it must have been created in a previous
@@ -121,7 +120,7 @@ SP run.
 Raiden Client Nodes Token Funding
 ---------------------------------
 Once the SP Token Contract is deployed or loaded, we can go ahead and fund the
-Raiden Client nodes with its tokens. This is done in a similar fashio to eth
+Raiden Client nodes with its tokens. This is done in a similar fashion to eth
 funding, in that it "overfunds" them by default, in order to avoid having to fund
 them on each run (which wastes time we could instead use to actually run the scenario).
 
@@ -130,28 +129,28 @@ UserDepositContract Token Balance check
 Next, we check if a UDC is requested in the scenario yaml, and, if so, check the
 UDC Token balance of the nodes (as these balances are also persisted between runs).
 
-UserDepositContract Token Allowance Update
+UserDepositContract Token Approval Update
 ------------------------------------------
-Depending on the scenario yaml's configuration, we update the allowance of the UDC for our
-Test Account address.
+Depending on the scenario yaml's configuration, we update the approved UDC amount
+for our Test Account address.
 
 Typically, each node would request the UDC tokens themselves, but this would require
-setting allowances for each node, resulting in multiple transaction calls (which is something
-we would like to avoid, for the sake of reducing set up time).
+setting approval amounts for each node, resulting in multiple transaction calls
+(which is something we would like to avoid, for the sake of reducing set up time).
 
-Instead, we set the allowance for the Test Account, and deposit the tokens from there, into
-each of the nodes.
+Instead, we set the approval amount for the Test Account, and deposit the tokens
+from there, into each of the nodes.
 
 ..Note::
 
-    The set `allowance` **must be less than or equal** to the minted amount of
+    The set `approval` **must be less than or equal** to the minted amount of
     UDC Tokens! Otherwise, the transactions for these deposits will not succeed,
     due to overstepping the allowed amount.
 
 UserDepositContract Token Deposits
 ----------------------------------
-Once the allowance has been updated, we can mint/deposit UDC Tokens for each of
-the nodes. This is done on a per-node basis. As stated before, we deposit more
+Once the approval amount has been updated, we can deposit UDC Tokens for each
+of the nodes. This is done on a per-node basis. As stated before, we deposit more
 than necessary by default, in order to avoid having to deposit on each scenario
 run.
 """
