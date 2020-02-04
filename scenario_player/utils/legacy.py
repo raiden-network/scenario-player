e20890e798fe30c9e8d5ddff0e0bfd0a48438e77
Dominik Schmid <dominik@brainbot.com>
2020-02-04 14:13:52 +0100

    invert condition

--- scenario_player/utils/legacy.py
+++ scenario_player/utils/legacy.py
@@ -223,7 +223,7 @@ def wait_for_txs(
                 if status is not None and status == 0:
                     raise ScenarioTxError(f"Transaction {txhash} failed.")
                 ## we want to add 2 blocks as confirmation
-                if tx["blockNumber"] + 2 >= web3.eth.getBlock("latest")['number']:
+                if tx["blockNumber"] + 2 < web3.eth.getBlock("latest")['number']:
                     txhashes.remove(txhash)
             time.sleep(0.1)
         time.sleep(1)
