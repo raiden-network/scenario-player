acc3c8b9f97a98dc5de6fbd8b36edde47c0dc11b
Karl Bartel <karl42@gmail.com>
2020-03-06 16:21:16 +0100

    Remove unused code

--- scenario_player/tasks/raiden_node.py
+++ scenario_player/tasks/raiden_node.py
@@ -13,18 +13,10 @@ class ProcessTask(Task):
         method = getattr(self._runner.node_controller[self._config], self._command)
         method()
 
-    def _handle_process(self, greenlet):  # pylint: disable=no-self-use
-        greenlet.join()
-        greenlet.get()
-
 
 class StartNodeTask(ProcessTask):
     _name = "start_node"
     _command = "start"
-
-    def _handle_process(self, greenlet):
-        # FIXME: Wait for port to become available and then stop blocking on the greenlet
-        super()._handle_process(greenlet)
 
 
 class StopNodeTask(ProcessTask):
