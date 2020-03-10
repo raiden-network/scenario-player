raiden-network/scenario-player

commit 3cde02c9e7ce4b1127e327d4a323f2563636bf45

Don't catch GreenletExit
Otherwise we can't kill the tasks when we want to exit.

Since we're catching `BaseException` here, we must make sure that the
exception is reraised. I've removed the unused option `about_on_fail` to
do that.

scenario_player/tasks/base.py
@@ -40,11 +40,7 @@ class Task:
     _name: str
 
     def __init__(
-        self,
-        runner: scenario_runner.ScenarioRunner,
-        config: Any,
-        parent: "Task" = None,
-        abort_on_fail=True,
+        self, runner: scenario_runner.ScenarioRunner, config: Any, parent: "Task" = None
     ) -> None:
         global _TASK_ID
 
@@ -53,7 +49,6 @@ class Task:
         self._runner = runner
         self._config = copy(config)
         self._parent = parent
-        self._abort_on_fail = abort_on_fail
         self._state = TaskState.INITIALIZED
         self.exception: Optional[BaseException] = None
         self.level: int = parent.level + 1 if parent else 0
@@ -70,21 +65,18 @@ class Task:
         self._start_time = time.monotonic()
         try:
             return self._run(*args, **kwargs)
-        except gevent.GreenletExit:
-            pass
         except BaseException as ex:
             self.state = TaskState.ERRORED
             log.exception("Task errored", task=self)
             self.exception = ex
-            if self._abort_on_fail:
-                raise
+            raise
         finally:
             self._stop_time = time.monotonic()
             self._runner.running_task_count -= 1
-            if self.state is TaskState.RUNNING:
-                runtime = self._stop_time - self._start_time
-                log.info("Task successful", task=self, runtime=runtime)
-                self.state = TaskState.FINISHED
+
+        runtime = self._stop_time - self._start_time
+        log.info("Task successful", task=self, runtime=runtime)
+        self.state = TaskState.FINISHED
 
     def _run(self, *args, **kwargs):  # pylint: disable=unused-argument,no-self-use
         gevent.sleep(1)

scenario_player/tasks/api_base.py
@@ -18,13 +18,9 @@ class RESTAPIActionTask(Task):
     _expected_http_status: Union[int, str] = "2.."
 
     def __init__(
-        self,
-        runner: scenario_runner.ScenarioRunner,
-        config: Any,
-        parent: "Task" = None,
-        abort_on_fail=True,
+        self, runner: scenario_runner.ScenarioRunner, config: Any, parent: "Task" = None
     ) -> None:
-        super().__init__(runner, config, parent, abort_on_fail)
+        super().__init__(runner, config, parent)
 
         self._expected_http_status = config.get("expected_http_status", self._expected_http_status)
         self._http_status_re = re.compile(f"^{self._expected_http_status}$")

scenario_player/tasks/blockchain.py
@@ -121,13 +121,9 @@ class AssertBlockchainEventsTask(Task):
     _name = "assert_events"
 
     def __init__(
-        self,
-        runner: scenario_runner.ScenarioRunner,
-        config: Any,
-        parent: "Task" = None,
-        abort_on_fail: bool = True,
+        self, runner: scenario_runner.ScenarioRunner, config: Any, parent: "Task" = None
     ) -> None:
-        super().__init__(runner, config, parent, abort_on_fail)
+        super().__init__(runner, config, parent)
 
         required_keys = ["contract_name", "event_name", "num_events"]
         all_required_options_provided = all(key in config.keys() for key in required_keys)
@@ -198,13 +194,9 @@ class AssertMSClaimTask(Task):
     _name = "assert_ms_claim"
 
     def __init__(
-        self,
-        runner: scenario_runner.ScenarioRunner,
-        config: Any,
-        parent: Task = None,
-        abort_on_fail: bool = True,
+        self, runner: scenario_runner.ScenarioRunner, config: Any, parent: Task = None
     ) -> None:
-        super().__init__(runner, config, parent, abort_on_fail)
+        super().__init__(runner, config, parent)
 
         required_keys = {"channel_info_key"}
         if not required_keys.issubset(config.keys()):

scenario_player/tasks/channels.py
@@ -95,7 +95,7 @@ class TransferTask(ChannelActionTask):
     def __init__(
         self, runner: scenario_runner.ScenarioRunner, config: Any, parent=None, abort_on_fail=True
     ) -> None:
-        super().__init__(runner, config, parent, abort_on_fail)
+        super().__init__(runner, config, parent)
         # Unique transfer identifier
         self.__class__._transfer_count += 1
         if str(self._config.get("identifier", "")).lower() == "generate":
@@ -123,13 +123,9 @@ class StoreChannelInfoTask(ChannelActionTask):
     _method = "get"
 
     def __init__(
-        self,
-        runner: scenario_runner.ScenarioRunner,
-        config: Any,
-        parent: Task = None,
-        abort_on_fail=True,
+        self, runner: scenario_runner.ScenarioRunner, config: Any, parent: Task = None
     ) -> None:
-        super().__init__(runner, config, parent, abort_on_fail)
+        super().__init__(runner, config, parent)
         if "key" not in config:
             raise ScenarioError('Required config "key" not found')
 

scenario_player/tasks/execution.py
@@ -16,13 +16,9 @@ class SerialTask(Task):
     _name = "serial"
 
     def __init__(
-        self,
-        runner: scenario_runner.ScenarioRunner,
-        config: Any,
-        parent: "Task" = None,
-        abort_on_fail=True,
+        self, runner: scenario_runner.ScenarioRunner, config: Any, parent: "Task" = None
     ) -> None:
-        super().__init__(runner, config, parent, abort_on_fail)
+        super().__init__(runner, config, parent)
         self._name = config.get("name")
 
         self._tasks: List = []

scenario_player/tasks/services.py
@@ -316,13 +316,9 @@ class AssertPFSIoUTask(RESTAPIActionTask):
     _url_template = "{pfs_url}/api/v1/_debug/ious/{source_address}"
 
     def __init__(
-        self,
-        runner: scenario_runner.ScenarioRunner,
-        config: Any,
-        parent: Task = None,
-        abort_on_fail: bool = True,
+        self, runner: scenario_runner.ScenarioRunner, config: Any, parent: Task = None
     ) -> None:
-        super().__init__(runner, config, parent, abort_on_fail)
+        super().__init__(runner, config, parent)
 
         if "source" not in config:
             raise ScenarioError("Not all required keys provided. Required: source ")

commit c1c3d273f6a7edd7b04257e7ce042825e44bcff2

Fix marking tasks as successful
I accidentally broke that in
https://github.com/raiden-network/scenario-player/pull/521 because I
missed the `return`.

scenario_player/tasks/base.py
@@ -64,7 +64,7 @@ class Task:
         self._runner.running_task_count += 1
         self._start_time = time.monotonic()
         try:
-            return self._run(*args, **kwargs)
+            return_val = self._run(*args, **kwargs)
         except BaseException as ex:
             self.state = TaskState.ERRORED
             log.exception("Task errored", task=self)
@@ -77,6 +77,7 @@ class Task:
         runtime = self._stop_time - self._start_time
         log.info("Task successful", task=self, runtime=runtime)
         self.state = TaskState.FINISHED
+        return return_val
 
     def _run(self, *args, **kwargs):  # pylint: disable=unused-argument,no-self-use
         gevent.sleep(1)